cd92befb6c4566fe91c7f24b34150665145ce56c
suggsgirl <suggsgirl@hotmail.com>
2021-05-07 11:03:26 +0100

    api added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
+import axios from "axios"; 
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+function handleResponse(response) {
+    console.log(response); 
+}
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for "${keyword}"...`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`; 
+    axios.get(apiUrl).then(handleResponse); 
   }
-  function handleKeywordChange(event) {
-    setKeyword(event.target.value);
-  }
+
+
+
+function handleKeywordChange(event) {
+  setKeyword(event.target.value);
+}
 
   return (
     <div className="Dictionary">
