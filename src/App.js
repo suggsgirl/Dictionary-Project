53c88eb586095ab4745e38be01b5c7741cd4958e
suggsgirl <suggsgirl@hotmail.com>
2021-05-07 16:12:53 +0100

    app styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">Dictionary</header>
-        <main> <Dictionary /> </main> 
+        <main> <Dictionary defaultKeyword="sunset"/> </main> 
         <footer className="App-footer"><small> Coded by Claire Phillips </small></footer>
       </div>
     </div>
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Meaning.css"; 
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
@@ -8,14 +9,12 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              <strong> Definition </strong>
+            <div className="definition"> 
               {definition.definition}
-              <br />
-              <strong> Example: </strong>
-              <em> {definition.example}</em>
-              <br />
-            </p>
+              </div>
+              <div className="example"> 
+             <em> {definition.example}</em>
+              </div>
             <Synonyms synonyms={definition.synonyms} />
           </div>
         );
