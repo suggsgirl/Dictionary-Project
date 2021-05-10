3013954676ea8b078ea4ace2c9b346fe6bd58702
suggsgirl <suggsgirl@hotmail.com>
2021-05-10 12:03:59 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import "./App.css";
-import Dictionary from "./Dictionary"; 
+import wordsworth from "./wordsworth.png";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Dictionary</header>
-        <main> <Dictionary defaultKeyword="sunset"/> </main> 
-        <footer className="App-footer"><small> Coded by Claire Phillips </small></footer>
+        <header className="App-header">
+          <img
+            src={wordsworth}
+            className="wordsworthLogo img-fluid"
+            alt="Wordsworth"
+          />{" "}
+          <br />
+          Wordsworths <br />
+          <div className="quote">  
+            "What we need is not the will to believe, but the wish to find out."{" "}
+          </div>
+        </header>
+        
+        <main>
+          {" "}
+          <Dictionary defaultKeyword="sunset" />{" "}
+        </main>
+        <footer className="App-footer">
+          <small>
+            {" "}
+            Coded by Claire Phillips and is {""}
+            <a
+              href="https://github.com/suggsgirl/Dictionary-Project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+          </small>
+        </footer>
       </div>
     </div>
   );
