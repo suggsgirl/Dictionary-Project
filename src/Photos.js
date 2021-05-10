cb421017379596f6bf557982c9f5bb93059c1adc
suggsgirl <suggsgirl@hotmail.com>
2021-05-10 11:16:32 +0100

    debugging

--- src/Photos.js
+++ src/Photos.js
@@ -3,7 +3,6 @@ import "./Photos.css";
 
 export default function Photos(props) {
   if (props.photos) {
-    
     return (
       <section className="Photos">
         <div className="row">
@@ -11,7 +10,11 @@ export default function Photos(props) {
             return (
               <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img src={photo.src.landscape} className="img-fluid" />{" "}
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt={""}
+                  />{" "}
                 </a>
               </div>
             );
